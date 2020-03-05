b4b5db3ac60f025411b5ee978cf5f986187de60c
euxn23 <euxn23@gmail.com>
2020-03-06 05:33:33 +0900

    fix: fix regexp of extracting url and tag

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,14 +1,15 @@
 export function extractUrl(text: string): string {
-  const matchResult = text.match(/(.*)\shttps:\/\/.*$/);
+  const matchResult = text.match(/^([\s\S]*)https:[\s\S]*$/);
   if (matchResult) {
     return matchResult[1];
   } else {
+    console.log(text);
     return text;
   }
 }
 
 export function extractNwontart(text: string): string {
-  const matchResult = text.match(/^(.*)\s?#ヌォンタート\s?(.*)$/);
+  const matchResult = text.match(/^([\s\S]*)#ヌォンタート([\s\S]*)$/);
   if (matchResult) {
     const extracted = [matchResult[1], matchResult[2]].join(' ');
     return extracted.split(' ').join('') ? extracted : '#ヌォンタート';
